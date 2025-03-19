a1c914a2670f5a34a3e07a3c9e180b129db632ff
Arbi Shehu <shehuarbi1@gmail.com>
2025-03-19 16:11:31 +0100

    fix immagine meditazione schermata iniziale e altro

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TimerPage from "./pages/TimerPage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
--- src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles/Loader.css"; // Assicurati di avere il file CSS
-
-const Loader = () => {
-  return <div className="loader"></div>;
-};
-
-export default Loader;
