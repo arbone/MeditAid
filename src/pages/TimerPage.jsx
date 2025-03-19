a99532c3794dcbb36c495eedc108f883dfed0bf0
Arbi Shehu <shehuarbi1@gmail.com>
2025-03-19 20:47:43 +0100

    finalizzazione e favicon

--- src/pages/TimerPage.jsx
+++ src/pages/TimerPage.jsx
@@ -49,6 +49,7 @@ const TimerPage = () => {
         {/* 🟢 CONTROLLI TIMER*/}
         <div className="controls-and-quotes">
           <div className="timer-controls">
+            
             {/* Play/Pausa */}
           <button className="control-button" onClick={toggleTimer}>
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
