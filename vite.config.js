d11c94ccdf9b2bd64a02c9e5be50bd6151f92ea0
Arbi Shehu <shehuarbi1@gmail.com>
2025-03-20 21:13:32 +0100

    fix package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // Nessun percorso specifico, così funziona in locale
   plugins: [react()],
-});
+})
